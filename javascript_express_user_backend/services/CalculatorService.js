846835ba187b99ab6652064842ac18c9dd9f66ba
Kenenna Okeke <43652027+KenennaOkeke@users.noreply.github.com>
2024-09-08 04:39:27 -0500

    Fine-tuning demo
    -simplification and easing the use in readme and frontend display

--- javascript_express_user_backend/services/CalculatorService.js
+++ javascript_express_user_backend/services/CalculatorService.js
@@ -40,30 +40,30 @@ service.calculate = async (
 		success: true,
 		data: {
 			"outputData": {
-				"min_bandwidth": [(
+				"min_bandwidth": [Math.round(
 					video_streaming_4k * Math.min(...video_streaming_4kRange) / 8 +
 					video_streaming_hd * Math.min(...video_streaming_hdRange) / 8 +
 					video_streaming_sd * Math.min(...video_streaming_sdRange) / 8 +
 					music_streaming * Math.min(...music_streamingRange) / 8 +
 					gaming * Math.min(...gamingRange) / 8 +
 					social_media * Math.min(...social_mediaRange) / 8
-				)],
-				"average_bandwidth": [(
+				, 2)],
+				"average_bandwidth": [Math.round(
 					video_streaming_4k * getAverage(video_streaming_4kRange) / 8 +
 					video_streaming_hd * getAverage(video_streaming_hdRange) / 8 +
 					video_streaming_sd * getAverage(video_streaming_sdRange) / 8 +
 					music_streaming * getAverage(music_streamingRange) / 8 +
 					gaming * getAverage(gamingRange) / 8 +
 					social_media * getAverage(social_mediaRange) / 8
-				)],
-				"max_bandwidth": [(
+				, 2)],
+				"max_bandwidth": [Math.round(
 					video_streaming_4k * Math.max(...video_streaming_4kRange) / 8 +
 					video_streaming_hd * Math.max(...video_streaming_hdRange) / 8 +
 					video_streaming_sd * Math.max(...video_streaming_sdRange) / 8 +
 					music_streaming * Math.max(...music_streamingRange) / 8 +
 					gaming * Math.max(...gamingRange) / 8 +
 					social_media * Math.max(...social_mediaRange) / 8
-				)]
+				, 2)]
 			}
 		},
 		errors: errors
